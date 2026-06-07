446cfe9ddb65134259d1514778a58d5c391fb436
milosilo-dev <lazerache@gmail.com>
2026-06-07 17:52:10 +0100

    chnages

--- guest/firmware/headers/gdt.h
+++ guest/firmware/headers/gdt.h
@@ -4,10 +4,12 @@ static uint64_t gdt64[] __attribute__((aligned(8))) = {
     0x0000000000000000,  // 0x00 null
     0x0000000000000000,  // 0x08 null
     0x0000000000000000,  // 0x10 null
-    0x00AF9A000000FFFF,  // 0x18 64-bit code ring-0  (L=1, D=0)
-    0x00CF92000000FFFF,  // 0x20 64-bit data ring-0
-    0x0000000000000000,  // 0x28 TSS low  (filled by gdt_set_tss)
-    0x0000000000000000,  // 0x30 TSS high (filled by gdt_set_tss)
+
+    0x00AF9A000000FFFF,  // 0x18 kernel code
+    0x00CF92000000FFFF,  // 0x20 kernel data
+
+    0x00AFFA000000FFFF,  // 0x28 user code (OK if DPL=3 inside descriptor)
+    0x00CFF2000000FFFF,  // 0x30 user data (OK if DPL=3 inside descriptor)
 };
 
 typedef struct {
--- guest/firmware/headers/paging.h
+++ guest/firmware/headers/paging.h
@@ -2,9 +2,10 @@
 
 #include <stdint.h>
 #include "../mem/heap.c"
-#define PAGE_PRESENT  (1 << 0)
-#define PAGE_WRITE    (1 << 1)
-#define PAGE_HUGE     (1 << 7)  // 2MB pages in PD entries
+#define PAGE_PRESENT    (1 << 0)
+#define PAGE_WRITE      (1 << 1)
+#define PAGE_HUGE       (1 << 7)  // 2MB pages in PD entries
+#define PAGE_USER       (1 << 2)
 
 #define PML4_ADDR 0x60000
 #define PDPT_ADDR 0x61000
@@ -33,9 +34,10 @@ static inline void paging_init(void) {
         uint64_t addr = (uint64_t)i * 0x200000ULL;
 
         pd[i] = (addr & 0x000FFFFFFFFFF000ULL)
-              | PAGE_PRESENT | PAGE_WRITE | PAGE_HUGE;
+              | PAGE_PRESENT | PAGE_WRITE | PAGE_HUGE | PAGE_USER;
     }
 
     // flush TLB safety (important in VM setups)
-    asm volatile("mov %0, %%cr3" :: "r"(pml4));
+    uint64_t cr3 = ((uint64_t)pml4 & 0x000FFFFFFFFFF000ULL);
+    asm volatile("mov %0, %%cr3" :: "r"(cr3));
 }
