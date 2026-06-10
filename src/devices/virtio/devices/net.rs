7367983319f3c79d7a6dc0caf9bd3b27f46e0fbb
milosilo-dev <lazerache@gmail.com>
2026-06-10 16:54:56 +0100

    More net stuff

--- src/devices/virtio/devices/net.rs
+++ src/devices/virtio/devices/net.rs
@@ -1,4 +1,4 @@
-use crate::devices::virtio::{devices::blk::BlkRequest, virtio::{VirtioDevice, VirtioGuestMemoryHandle}};
+use crate::devices::virtio::{devices::blk::BlkRequest, virtio::{VirtioDevice, VirtioGuestMemoryHandle, VirtioQueue}};
 
 struct VirtioNetConfig {
 
@@ -14,7 +14,7 @@ impl VirtioNetConfig {
     }
 }
 
-struct Packetdesc{
+struct PacketDesc{
     flags: u8,                  // Bit 0: Needs checksum; Bit 1: Received packet has valid data; Bit 2: If VIRTIO_NET_F_RSC_EXT was negotiated
     segmentation_offload: u8,   // 0:None 1:TCPv4 3:UDP 4:TCPv6 0x80:ECN
     desc_length: u16,         // Size of desc to be used during segmentation.
@@ -24,7 +24,7 @@ struct Packetdesc{
     buffer_count: u16,          // Used when merging buffers.
 }
 
-impl Packetdesc{
+impl PacketDesc{
     fn new(base_ptr: u64, guest_memory: &VirtioGuestMemoryHandle) -> Self{
         Self {
             flags: guest_memory.read_byte(base_ptr),
@@ -50,6 +50,61 @@ impl NetVirtio{
             config: VirtioNetConfig::new() 
         }
     }
+
+    // Send Packet to client
+    fn tick_rx_queue(&mut self, queue: &mut VirtioQueue) -> bool {
+        let Some(guest_memory) = self.guest_memory.as_mut() else {
+            return false;
+        };
+
+        while let Some(head) = queue.pop_avail(guest_memory) {
+            let desc = queue.get_descriptor(guest_memory, head);
+
+            if desc.flags & 1 != 0 {
+                // Has seperate descriptor for packet data
+                todo!();
+            }
+
+            // write virtio_net_hdr
+            guest_memory.write_guest_memory(desc.addr, &header_bytes);
+
+            // write packet after header
+            guest_memory.write_guest_memory(desc.addr + 12, ethernet_frame);
+
+            queue.push_used(
+                guest_memory,
+                head,
+                12 + ethernet_frame.len() as u32
+            );
+        }
+        true
+    }
+
+    // Handle packet from client
+    fn tick_tx_queue(&mut self, queue: &mut VirtioQueue) -> bool {
+        let Some(guest_memory) = self.guest_memory.as_mut() else {
+            return false;
+        };
+
+        while let Some(head) = queue.pop_avail(guest_memory) {
+            let desc = queue.get_descriptor(&guest_memory, head);
+            let header = PacketDesc::new(desc.addr, guest_memory);
+
+            let packet_addr = if desc.flags & 1 != 0 {
+                // Has seperate descriptor for packet data
+                let data_desc = queue.get_descriptor(&guest_memory, desc.next);
+                data_desc.addr
+            } else {
+                desc.addr + 12
+            }
+
+            let mut packet = vec![0u8; desc.len as usize];
+            guest_memory.read_guest_memory(packet_addr, &mut packet);
+
+            queue.push_used(guest_memory, head, desc.len);
+        }
+        true
+    }
 }
 
 impl VirtioDevice for NetVirtio {
@@ -65,24 +120,16 @@ impl VirtioDevice for NetVirtio {
         self.guest_memory = Some(guest_memory);
     }
 
-    fn tick(&mut self, queue_sel: usize, queue: &mut crate::devices::virtio::virtio::VirtioQueue) -> bool {        
-        let Some(guest_memory) = self.guest_memory.as_mut() else {
-            return false;
-        };
-
-        while let Some(head) = queue.pop_avail(guest_memory) {
-            let desc = queue.get_descriptor(&guest_memory, head);
-
-            if desc.flags & 1 == 0 || desc.len != 16 {
-                eprintln!("blk: SKIP hdr flags={:#06x} len={}", desc.flags, desc.len);
-                queue.push_used(guest_memory, head, 0);
-                continue;
+    fn tick(&mut self, queue_sel: usize, queue: &mut VirtioQueue) -> bool {        
+        match queue_sel{
+            0 => {
+                self.tick_rx_queue(queue)
             }
-
-            let packet_hdr = Packetdesc::new(desc.addr, guest_memory);
+            1 => {
+                self.tick_tx_queue(queue)
+            }
+            _ => true
         }
-
-        true
     }
 
     fn read_config(&self, length: usize) -> Vec<u8> {
